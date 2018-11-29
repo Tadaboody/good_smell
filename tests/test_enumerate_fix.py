38e2e85f938b23d91122d26bf7c2d15315d8b66b
Tomer Keren <tomer.keren.dev@gmail.com>
2018-11-29 23:20:42 +0200

    Add failing tests for #4

--- tests/test_enumerate_fix.py
+++ tests/test_enumerate_fix.py
@@ -9,13 +9,43 @@ a = [0]
 for i in range(len(a)):
     print(a[i])
 """,
-          """
+                 """
 b = [1]
 for i in range(len(a + b)):
     print(i)
 """]
 
-@pytest.mark.parametrize("source",valid_sources)
+
+@pytest.mark.parametrize("source", valid_sources)
 def test_range_len_fix(source):
-    assert not match(r'for \w+ in range\(len\(.+\)\):', fix_smell(source))
+    assert not match(r'for \w+ in range\(len\(.+\)\):',
+                     fix_smell(source))
+
+
+def test_range_len_assert_fix():
+    source = """seq = [0]
+for i in range(len(seq)):
+    a = seq[i]
+    print(a)
+    """
+    fixed_source = """seq = [0]
+for i,a in enumerate(seq):
+    print(a)
+    """
+    assert fix_smell(source) == fixed_source
+
+
+def test_empty_body():
+    """Assert that when creating an empty body it is subsituted with pass"""
+    source = """seq = [0]
+for i in range(len(seq)):
+    a = seq[i]
+    print(a)
+    """
+    fixed_source = """
+seq = [0]
+for i,a in enumerate(seq):
+    pass
+    """
+    assert fix_smell(source) == fixed_source
 
