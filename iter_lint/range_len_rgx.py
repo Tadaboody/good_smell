95f6da42194540235f8e6fc9bac229e528681b99
Omer Belsky <omerbelsky3@gmail.com>
2018-12-02 22:13:28 +0200

    fixed the issue with the regex (at least hopefully)

--- iter_lint/range_len_rgx.py
+++ iter_lint/range_len_rgx.py
@@ -9,22 +9,18 @@ class RangeLenRGX(LintSmell):
 
     def fix_smell(self) -> str:
         fixed_source = self.source_code.rstrip() + os.linesep
-        for for_match in re.finditer(r"(\s*)for (\w+) in range\(len\((.+)\)\):", self.source_code):
-            indent = for_match.group(1)
-            index = for_match.group(2)
-            seq = for_match.group(3)
-            content_rgx = indent + "for " + index + r" in range\(len\(" + seq + r"\)\):\n((" + indent + r"(\s).+\n)*)"
-            print(content_rgx)
-            print(fixed_source)
+        for for_match in re.finditer(r"\n*(?<!#)( *|\t*)for (\w+) in range\(len\((.+)\)\):", self.source_code):
+            indent, index, seq = for_match.groups()
+            for_statement = indent + "for " + index + " in range(len(" + seq + ")):" + os.linesep
+            content_rgx = indent + "for " + index + r" in range\(len\(" + seq + r"\)\):" + os.linesep + "((" + indent + r"(\s+).+" + os.linesep + ")*)"
             content = re.search(content_rgx, fixed_source).group(1)
-            print(content)
-            fixed_content = re.sub(seq + "[" + index + "]", "elm", content)
-            fixed_content = "{}for {}, elm in enumerate({}):\n".format(indent, index, seq) + fixed_content
-            fixed_source = re.sub(fixed_content, content, fixed_source)
+            fixed_content = content.replace(seq + "[" + index + "]", "elm")
+            fixed_content = "{}for {}, elm in enumerate({}):".format(indent, index, seq) + os.linesep + fixed_content
+            fixed_source = fixed_source.replace(for_statement + content, fixed_content)
         return fixed_source
 
 if __name__ == "__main__":
-    source = """    for i in range(len(a)):\n        print(a[i])\n"""
+    source = """    for i in range(len(a)):""" + os.linesep + """        print(a[i])"""
     print(source)
     rlr = RangeLenRGX(source)
     print(rlr.fix_smell())
