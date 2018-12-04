1317547349771fe359d5edbf5930a60b77d59202
Omer Belsky <omerbelsky3@gmail.com>
2018-12-04 12:24:54 +0200

    Working implementation of sniffer using regex. (Still need to work on nested fors)

--- iter_lint/range_len_rgx.py
+++ iter_lint/range_len_rgx.py
@@ -5,22 +5,39 @@ import os
 class RangeLenRGX(LintSmell):
 
     def check_for_smell(self) -> bool:
-        return bool(re.search(self.source_code, r"for \w+ in range\(len\(.+\)\):"))
+        return bool(re.search(self.source_code, r"\n*( *|\t*)for (\w+) in range\(len\((.+)\)\):"))
 
     def fix_smell(self) -> str:
         fixed_source = self.source_code.rstrip() + os.linesep
-        for for_match in re.finditer(r"\n*(?<!#)( *|\t*)for (\w+) in range\(len\((.+)\)\):", self.source_code):
+        for for_match in re.finditer(r"(?=\n*|\r*)( *|\t*)for (\w+) in range\(len\((.+)\)\):", self.source_code):  # https://regex101.com/r/ib52OT/1
             indent, index, seq = for_match.groups()
-            for_statement = indent + "for " + index + " in range(len(" + seq + ")):" + os.linesep
-            content_rgx = indent + "for " + index + r" in range\(len\(" + seq + r"\)\):" + os.linesep + "((" + indent + r"(\s+).+" + os.linesep + ")*)"
+            for_statement = indent + "for " + re.escape(index) + " in range(len(" + re.escape(seq) + ")):" + os.linesep
+            content_rgx = indent + "for " + re.escape(index) + r" in range\(len\(" + re.escape(seq) + r"\)\):" + os.linesep + "((" + indent + r"(\s+).+" + os.linesep + ")*)"
             content = re.search(content_rgx, fixed_source).group(1)
-            fixed_content = content.replace(seq + "[" + index + "]", "elm")
-            fixed_content = "{}for {}, elm in enumerate({}):".format(indent, index, seq) + os.linesep + fixed_content
-            fixed_source = fixed_source.replace(for_statement + content, fixed_content)
-        return fixed_source
+            #content = RangeLenRGX(content).fix_smell()
+            assign = re.search(r"([^\s]+)\s*=\s*(.*)(" + re.escape(seq) + r"\[" + re.escape(index) + r"\]" + r")(.*)", content)  # https://regex101.com/r/CnibMS/1
+            elm = "elm"
+            i = 1
+            while re.search(elm, fixed_source):
+                elm = "elm" + str(i)
+            fixed_content = content
+            if assign and assign.group(2).strip() == assign.group(4).strip() == "":
+                elm = assign.group(1)
+                fixed_content = content.replace(assign.group(0), "")
+                fixed_content = os.linesep.join([line for line in fixed_content.splitlines() if line.strip() != ""])
+            fixed_content = fixed_content.replace(seq + "[" + index + "]", elm)
+            if fixed_content.strip() == "":
+                fixed_content += indent + "    pass"
+            fixed_content = "{}for {}, {} in enumerate({}):".format(indent, index, elm, seq) + os.linesep + fixed_content
+            fixed_source = fixed_source.replace(for_statement + content, fixed_content + os.linesep)
+        return (fixed_source).replace("\r", "")
 
 if __name__ == "__main__":
-    source = """    for i in range(len(a)):""" + os.linesep + """        print(a[i])"""
+    source ="""seq = [0]
+for i in range(len(seq)):
+    a = seq[i]
+    print(a)
+    """
     print(source)
     rlr = RangeLenRGX(source)
     print(rlr.fix_smell())
