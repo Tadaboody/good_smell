67efd8b860e0a19b979fd9fcd18a86c463cbef8b
Tomer Keren <tomer.keren.dev@gmail.com>
2018-12-07 12:09:02 +0200

    Add the backport of dataclasses

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 import sys
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner==4.2'] if needs_pytest else []
+dataclasses = ['dataclasses==0.6'] if sys.version_info < (3, 7) else [] # Backport of dataclasses from py3.7
 
 setup(
     name='good_smell',
@@ -13,7 +14,7 @@ setup(
         'fire==0.1.3',
         'astor==0.7.1',
         'astpretty==1.4.0',
-    ],
+    ] + dataclasses,
     setup_requires=[] + pytest_runner,
     tests_require=['pytest==4.0.1',
                    'mccabe==0.6.1', 'pytest-mccabe==0.1',
