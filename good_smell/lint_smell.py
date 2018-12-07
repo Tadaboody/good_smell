3ceb0783433bc1baced0a985b4b32ed597b38c15
Tomer Keren <tomer.keren.dev@gmail.com>
2018-12-07 12:37:31 +0200

    Declare class to encapsulate watnings

--- good_smell/lint_smell.py
+++ good_smell/lint_smell.py
@@ -1,18 +1,22 @@
 import abc
 import os
+from good_smell import SmellWarning
+
 
 class LintSmell(abc.ABC):
-    """A class to represent the sniffing instructions for the linter"""
+    """Abstract Base class to represent the sniffing instructions for the linter"""
 
     def __init__(self, source_code: str, start_line=0, end_line=None):
         self.start_line = start_line
         self.end_line = end_line or len(source_code.splitlines())
-        self.source_code = os.linesep.join(source_code.splitlines()[start_line:end_line])
+        self.source_code = os.linesep.join(
+            source_code.splitlines()[start_line:end_line])
 
     @abc.abstractmethod
-    def check_for_smell(self) -> bool:
+    def check_for_smell(self) -> SmellWarning:
         """Check if the smell occurs between `starting_line` and `end_line` in `source_code`"""
 
     @abc.abstractmethod
     def fix_smell(self) -> str:
         """Return a fixed version of the code without the code smell"""
+
---
+++ good_smell/smell_warning.py
@@ -0,0 +1,16 @@
+import dataclasses
+
+FLAKE8_FORMAT = '{path:s}:{row:d}:{col:d}:{code:s}{text:s}'
+
+
+@dataclasses.dataclass(frozen=True)  # pylint: disable=too-few-public-methods
+class SmellWarning:
+    """Class to represent a warning message about a smell"""
+    code: str
+    row: int
+    col: int
+    path: str
+    msg: str
+
+    def warning_string(self, formatter: str = FLAKE8_FORMAT):
+        return formatter.format(**dataclasses.asdict(self))
