77341f9749caae2ec0ada75653b2fd9d620e57a7
Tomer Keren <tomer.keren.dev@gmail.com>
2018-12-08 01:25:36 +0200

    Add command to only send warnings about the smells

--- good_smell/main.py
+++ good_smell/main.py
@@ -1,28 +1,39 @@
 from pathlib import Path
 
-import astor
 from fire import Fire
+from typing import Type, Iterable
 
-from good_smell import LintSmell, RangeLenSmell, NestedFor
+from good_smell import LintSmell, RangeLenSmell, NestedFor, SmellWarning
+
+
+def print_smell_warnings(path: str):
+    """Prints any warning messages about smells"""
+    print('\n'.join(warning.warning_string()
+                    for warning in smell_warnings(Path(path))))
+
+
+def smell_warnings(path: Path) -> Iterable[SmellWarning]:
+    for smell in (NestedFor,):
+        yield from smell(source_code=path.read_text(), path=str(path)).check_for_smell()
 
 
 def print_fixed_smell(path: str, starting_line: int = 0, end_line: int = None):
     """Prints a fixed version of `source`"""
-    path = Path(path)
-    source = path.read_text()
+    pathlib_path = Path(path)
+    source = pathlib_path.read_text()
     print(fix_smell(source, starting_line, end_line))
 
 
-def fix_smell(source: str, starting_line: int = 0, end_line: int = None) -> str:
+def fix_smell(source: str, starting_line: int = 0, end_line: int = None, path: str = None) -> str:
     """Returns a fixed version of `source`"""
-    smell: LintSmell
+    smell: Type[LintSmell]
     for smell in (RangeLenSmell, NestedFor):
         source = smell(source, starting_line, end_line).fix_smell()
     return source
 
 
 def main():
-    Fire(print_fixed_smell)
+    Fire({'fix': print_fixed_smell, 'warn': print_smell_warnings})
 
 
 if __name__ == '__main__':
--- good_smell/smell_warning.py
+++ good_smell/smell_warning.py
@@ -1,6 +1,7 @@
 import dataclasses
 
 FLAKE8_FORMAT = '{path:s}:{row:d}:{col:d}:{code:s}{text:s}'
+FLAKE8_FORMAT = '{path}:{row}:{col} {code} {msg}'
 
 
 @dataclasses.dataclass(frozen=True)  # pylint: disable=too-few-public-methods
