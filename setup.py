1731c09f64ad74b2f711da88986c094db16b4920
Tomer Keren <tomer.keren.dev@gmail.com>
2018-12-26 23:34:26 +0200

    Add an extra requirement point for tests reqs

--- setup.py
+++ setup.py
@@ -9,9 +9,15 @@ dataclasses = (
     [] if sys.version_info < (3, 7) else []
 )  # Backport of dataclasses from py3.7
 
+tests_require = [
+    "pytest==4.0.1",
+    "mccabe==0.6.1",
+    "pytest-mccabe==0.1",
+    "autopep8==1.4.3",
+]
 setup(
     name="good_smell",
-    version="0.2",
+    version="0.3",
     py_modules=["good_smell"],
     packages=["good_smell"],
     install_requires=[
@@ -23,14 +29,10 @@ setup(
     ]
     + dataclasses,
     setup_requires=[] + pytest_runner,
-    tests_require=[
-        "pytest==4.0.1",
-        "mccabe==0.6.1",
-        "pytest-mccabe==0.1",
-        "autopep8==1.4.3",
-    ],
+    tests_require=tests_require,
     entry_points={
         "console_scripts": ["good_smell=good_smell:main"],
         "flake8.extension": "SML=good_smell:GoodSmellFlake8",
     },
+    extras_require={"test": tests_require},
 )
