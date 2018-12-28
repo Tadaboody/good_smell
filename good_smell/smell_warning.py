6da1b3c391980ec2c77bd219c0f3401c8324b116
Tomer Keren <tomer.keren.dev@gmail.com>
2018-12-29 00:51:41 +0200

    Add symbol for smells

--- good_smell/smell_warning.py
+++ good_smell/smell_warning.py
@@ -2,6 +2,7 @@ import dataclasses
 
 FLAKE8_FORMAT = "{path:s}:{row:d}:{col:d}:{code:s}{text:s}"
 FLAKE8_FORMAT = "{path}:{row}:{col} {code} {msg}"
+PYLINT_FORMAT = "{path}:{line}:{column}: {msg_id}: {msg} ({symbol})"
 
 
 @dataclasses.dataclass(frozen=True)  # pylint: disable=too-few-public-methods
@@ -13,6 +14,7 @@ class SmellWarning:
     col: int
     path: str
     msg: str
+    symbol: str
 
-    def warning_string(self, formatter: str = FLAKE8_FORMAT):
+    def warning_string(self, formatter: str = PYLINT_FORMAT):
         return formatter.format(**dataclasses.asdict(self))
