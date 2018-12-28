3c1204507542ac0a0a7bc5e4c0e7017903b5aa7d
Tomer Keren <tomer.keren.dev@gmail.com>
2018-12-28 17:57:03 +0200

    Move smells to a subdir
    Closes #28

--- good_smell/__init__.py
+++ good_smell/__init__.py
@@ -1,8 +1,6 @@
 from .smell_warning import SmellWarning
 from .lint_smell import LintSmell
-from .range_len_fix import RangeLenSmell
-from .nested_for import NestedFor
-
-implemented_smells = (RangeLenSmell, NestedFor)
+from .smells import implemented_smells
+from . import smells # Allow importing good_smell.smells
 from .flake8_ext import GoodSmellFlake8
 from .main import fix_smell, print_fixed_smell, main
---
+++ good_smell/smells/__init__.py
@@ -0,0 +1,4 @@
+from .range_len_fix import RangeLenSmell
+from .nested_for import NestedFor
+
+implemented_smells = (RangeLenSmell, NestedFor)
