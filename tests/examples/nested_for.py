3fbe374f45f168b806be26101b23afa34ab8851e
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-01 00:54:10 +0200

    Don't flatten a nested for with dependencies
    Closes #26

--- tests/examples/nested_for.py
+++ tests/examples/nested_for.py
@@ -6,9 +6,9 @@ for i in seq_a:
     for j in seq_b:
         print(i, j)
 # ==>
+import itertools
 seq_a = [0]
 seq_b = range(10)
-import itertools
 for i, j in itertools.product(seq_a, seq_b):
     print(i, j)
 
@@ -34,3 +34,12 @@ for i in seq_a:
     for j in seq_b:
         print(i, j)
     print(i)
+#: Don't flatten a nested for with dependencies (#26)
+# None
+for num in range(1, 5):
+    for digits in range(1, 10 ** num):
+        pass
+# ==>
+for num in range(1, 5):
+    for digits in range(1, 10 ** num):
+        pass
