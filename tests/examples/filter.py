28b2d366aedb322778b1c0661762a03f4769c9bd
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-01 00:03:55 +0200

    Add generator extension test

--- tests/examples/filter.py
+++ tests/examples/filter.py
@@ -30,3 +30,11 @@ for i in range(10):
     if pred(i):
         print(1)
     print(2)
+#: Merge into existing expr
+# filter-iterator
+for i in (x * 2 for x in range(2)):
+    if pred(i):
+        pass
+# ==>
+for i in (x * 2 for x in range(2) if pred(x)):
+    pass
