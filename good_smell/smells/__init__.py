28b4a0dee5587345ed1d100cd7c57a68d838d560
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-02 03:43:39 +0200

    add YieldFrom smell Closes: #51 Progresses : #

--- good_smell/smells/__init__.py
+++ good_smell/smells/__init__.py
@@ -1,5 +1,6 @@
 from .range_len_fix import RangeLenSmell
 from .nested_for import NestedFor
 from .filter import FilterIterator
+from .yield_from import YieldFrom
 
-implemented_smells = (RangeLenSmell, NestedFor, FilterIterator)
+implemented_smells = (RangeLenSmell, NestedFor, FilterIterator, YieldFrom)
---
+++ good_smell/smells/yield_from.py
@@ -0,0 +1,40 @@
+from good_smell import AstSmell, LoggingTransformer
+import ast
+import pprint
+
+
+class YieldFrom(AstSmell):
+    """Checks for adjacent nested fors and replaces them with itertools.product"""
+
+    @property
+    def transformer_class(self):
+        return YieldFromTransformer
+
+    @property
+    def warning_message(self):
+        return "Consider using yield from instead of yield inside of a for loop"
+
+    @property
+    def symbol(self):
+        return "yield-from"
+
+
+class YieldFromTransformer(LoggingTransformer):
+    """NodeTransformer that goes visits all the yields in fors and replaces them
+    with yield from"""
+
+    def visit_For(self, node: ast.For):
+        yield_from = ast.Expr(
+            value=ast.YieldFrom(node.iter)
+        )
+        return ast.fix_missing_locations(yield_from)
+
+    @staticmethod
+    def is_smelly(node: ast.AST):
+        """Check if the node is a yield inside a for"""
+        return (
+            isinstance(node, ast.For)
+            and len(node.body) != 1
+            and isinstance(node.body[0], ast.Expr)
+            and isinstance(node.body[0].value, ast.Yield)
+        )
