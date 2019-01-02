3ea229de95e3050ce1aa79fab9188bf790b7df03
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-02 02:04:19 +0200

    Bump version to 0.12.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ tests_require = [
 ]
 setup(
     name="good_smell",
-    version="0.11.0",
+    version="0.12.0",
     packages=find_packages(exclude=("tests",)),
     setup_requires=[] + pytest_runner,
     install_requires=[
