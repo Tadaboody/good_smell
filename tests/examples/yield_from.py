af54151f3def88aed6f743e63bcf1c8013cd9f32
Adam Goldschmidt <adamgold7@gmail.com>
2019-01-05 13:40:55 +0200

    put tests inside a function, fix docstring

--- tests/examples/yield_from.py
+++ tests/examples/yield_from.py
@@ -1,8 +1,12 @@
 #: use yield from iter instead for x in iter -> yield x
 # yield-from
-seq = range(10)
-for x in seq:
-    yield x
+def test1():
+    seq = range(10)
+    for x in seq:
+        yield x
+test1()
 # ==>
-seq = range(10)
-yield from seq
+def test2():
+    seq = range(10)
+    yield from seq
+test2()
