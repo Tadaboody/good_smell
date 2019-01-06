365b7443b308421ce0e95a51ceb45ddedf1b303e
Tomer Keren <tomer.keren.dev@gmail.com>
2019-01-06 20:55:45 +0200

    Revert "put yield in functions"
    This reverts commit a33d89a42538f36b67aaa63e918df6da35b5e4a8.

--- tests/examples/yield_from.py
+++ tests/examples/yield_from.py
@@ -1,12 +1,8 @@
 #: use yield from iter instead for x in iter -> yield x
 # yield-from
-def test1():
-    seq = range(10)
-    for x in seq:
-        yield x
-test1()
+seq = range(10)
+for x in seq:
+    yield x
 # ==>
-def test2():
-    seq = range(10)
-    yield from seq
-test2()
+seq = range(10)
+yield from seq
