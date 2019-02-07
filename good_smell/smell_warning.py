53c1b10a178d531eb9432d1da6533f26b46960e7
Tomer Keren <tomer.keren.dev@gmail.com>
2019-02-07 12:16:22 +0200

    Remove dataclasses dependency

--- good_smell/smell_warning.py
+++ good_smell/smell_warning.py
@@ -1,11 +1,14 @@
-import dataclasses
+from typing import NamedTuple
 
 FLAKE8_FORMAT = "{path}:{row}:{col} {symbol} {msg}"
 PYLINT_FORMAT = "{path}:{line}:{column}: {msg} ({symbol})"
 
 
-@dataclasses.dataclass(frozen=True)  # pylint: disable=too-few-public-methods
-class SmellWarning:
+def to_dict(namedtuple: NamedTuple) -> dict:
+    return dict(zip(namedtuple._fields, list(namedtuple)))
+
+
+class SmellWarning(NamedTuple):
     """Class to represent a warning message about a smell"""
 
     row: int
@@ -15,4 +18,4 @@ class SmellWarning:
     symbol: str
 
     def warning_string(self, formatter: str = PYLINT_FORMAT):
-        return formatter.format(**dataclasses.asdict(self))
+        return formatter.format(**to_dict(self))
