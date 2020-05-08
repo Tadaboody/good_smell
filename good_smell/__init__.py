a953836a77d20cd4376929131c7e84ab21a58a54
Tomer Keren <tomer.keren.dev@gmail.com>
2020-05-08 14:54:16 +0300

    Remove outdated import

--- good_smell/__init__.py
+++ good_smell/__init__.py
@@ -5,4 +5,4 @@ from .ast_smell import AstSmell, LoggingTransformer
 from .smells import implemented_smells
 from .main import fix_smell, print_fixed_smell, main, smell_warnings
 from . import smells  # Allow importing good_smell.smells
-from .flake8_ext import LintingFlake8, FormattingFlake8
+from .flake8_ext import LintingFlake8
