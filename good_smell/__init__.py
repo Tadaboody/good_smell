bf3e59c66bf9edd2ffa59587b74f9cd627a90e19
Tomer Keren <tomer.keren.dev@gmail.com>
2020-07-16 13:11:51 +0300

    :bug: Export entry points again

--- good_smell/__init__.py
+++ good_smell/__init__.py
@@ -1,4 +1,11 @@
 # flake8:noqa
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
+__version__ = metadata.version("good-smell")
+
 from .smell_warning import SmellWarning
 from .lint_smell import LintSmell
 from .ast_smell import AstSmell, LoggingTransformer
