39f88a988b2516aaf170419ecde3cb495ae57df6
Tomer Keren <tomer.keren.dev@gmail.com>
2020-08-14 22:02:43 +0300

    :bug: Dont crash flake8

--- good_smell/flake8_ext.py
+++ good_smell/flake8_ext.py
@@ -1,7 +1,17 @@
 import ast
 from typing import Generator, Tuple
+import warnings as python_warnings
 
 from good_smell import SmellWarning, implemented_smells, __version__
+
+
+def get_traceback():
+    import traceback
+    import sys
+    import os
+
+    _, _, tb = sys.exc_info()
+    return os.linesep.join(traceback.format_tb(tb))
 
 
 class LintingFlake8:
@@ -17,9 +27,20 @@ class LintingFlake8:
 
     def run(self) -> Generator[Tuple[int, int, str, str], None, None]:
         for num, smell in enumerate(implemented_smells):
-            warnings = smell(
-                transform=False, tree=self.tree, path=self.filename
-            ).check_for_smell()
+            try:
+                warnings = smell(
+                    transform=False, tree=self.tree, path=self.filename
+                ).check_for_smell()
+            except Exception:
+                ISSUE_LINK = "https://github.com/Tadaboody/good_smell/issues/new?assignees=&labels=bug%2C+new&template=bug_report.md"
+                python_warnings.warn(
+                    f"""good-smell crashed, please open an issue to {ISSUE_LINK} and include the following stack trace:
+```py
+{get_traceback()}
+```
+                 """
+                )
+                continue
             warning: SmellWarning
             yield from (
                 (
