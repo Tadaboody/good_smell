bf863e02ad715aa461a071065fe19a33648ccf77
Tomer Keren <tomer.keren.dev@gmail.com>
2021-03-26 11:55:52 +0300

    join-literal: Warn when using str.join on a constant list

--- good_smell/smells/__init__.py
+++ good_smell/smells/__init__.py
@@ -1,6 +1,7 @@
-from .range_len_fix import RangeLenSmell
-from .nested_for import NestedFor
 from .filter import FilterIterator
+from .join_literal import JoinLiteral
+from .nested_for import NestedFor
+from .range_len_fix import RangeLenSmell
 from .yield_from import YieldFrom
 
-implemented_smells = (RangeLenSmell, NestedFor, FilterIterator, YieldFrom)
+implemented_smells = (RangeLenSmell, NestedFor, FilterIterator, YieldFrom, JoinLiteral)
